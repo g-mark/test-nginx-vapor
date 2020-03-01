9df331714aafcbaaa4e1eaf12a61eed578185354
Steven Grosmark <steven.grosmark@weightwatchers.com>
2020-03-01 11:31:29 -0500

    Add links to static and dynamic pages & routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,14 +3,27 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // "It works" page
-    router.get { req in
-        return try req.view().render("welcome")
+    router.get { req -> EventLoopFuture<View> in
+        let test = req.query[String.self, at: "test"] ?? ""
+        return try req.view()
+            .render(
+                "welcome",
+                [
+                    "test": test
+                ]
+            )
     }
     
     // Says hello
     router.get("hello", String.parameter) { req -> Future<View> in
-        return try req.view().render("hello", [
-            "name": req.parameters.next(String.self)
-        ])
+        let test = req.query[String.self, at: "test"] ?? ""
+        return try req.view()
+            .render(
+                "hello",
+                [
+                    "name": req.parameters.next(String.self),
+                    "test": test
+                ]
+            )
     }
 }
